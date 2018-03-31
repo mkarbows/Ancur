903957753dc2e2e87ac2002d15f967ba4683e5db
Megan Karbowski <mck064@gmail.com>
2018-03-31 13:11:51 -0700

    messing around with simpler options

--- source/ancur/app.js
+++ source/ancur/app.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const app = express();
+// const path = require("path");
 const Stampery = require('stampery');
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 var stampery = new Stampery('342463c4-3cbe-4d63-b1fe-48d7a9c13c3d');
 app.get('/', function(req, res, next) {
-  // res.send('hihi')
   var stampery = new Stampery('342463c4-3cbe-4d63-b1fe-48d7a9c13c3d');
-  // reqs.params.file is coming from the file that is uploaded in app.js
   const hash = stampery.hash('the piano has been drinking ' + Math.random());
   stampery.stamp(hash).then((stamp) => {
     // res.status(200).send(stamp);
@@ -21,13 +19,8 @@ app.get('/', function(req, res, next) {
 
 });
 
-// const hash = stampery.hash('the piano has been drinking ' + Math.random());
-// stampery.stamp(hash).then((stamp) => {
-//   return console.log(stamp);
-// }).catch((err) => {
-//   return console.error(err);
+// app.get('/', function(req, res, next) {
+//   res.sendFile(path.join(__dirname+'index.html'));
 // });
-
-
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
